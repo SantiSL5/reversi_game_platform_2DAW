e0fc289b92b33e04ea509ad4295bff279a2fd611
SantiSL5 <santiagosolerllin@gmail.com>
2021-11-15 21:03:26 +0100

    Most functionality implemented

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
-    let boardLayout = [
+    var boardLayout = [
         [0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
@@ -7,7 +7,7 @@ window.addEventListener('load', () => {
         [0, 0, 0, 1, 2, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0]
     ]
     const colors = [
         '#2196f3',
@@ -15,7 +15,21 @@ window.addEventListener('load', () => {
         '#ffeb3b',
         '#74ff1d'
     ]
+
+    const directions = [
+        [0,1],
+        [1,1],
+        [1,0],
+        [1,-1],
+        [0,-1],
+        [-1,-1],
+        [-1,0],
+        [-1,1]
+    ]
+
     const board = document.getElementById('board');
+    const playerScore = document.getElementById('playerScore');
+    const AIScore = document.getElementById('AIScore');
     const prefixes = ['-o-', '-ms-', '-moz-', '-webkit-'];
     let turn='black';
 
@@ -65,19 +79,28 @@ window.addEventListener('load', () => {
         cells=document.getElementsByClassName('cell');
         for (let i = 0; i < cells.length; i++) {
             cells[i].addEventListener('click', () => {
-                if (!cells[i].querySelector(".tile")) {
-                    if (turn == 'black') {
-                        turn='white';
-                        tilexy=cells[i].id.split('cell',2)[1].split('-',2);
-                        boardLayout[parseInt(tilexy[0])][parseInt(tilexy[1])]=2;
-                        updateBoard();
-                        console.log(cells[i].id.split('cell',2)[1].split('-',2));
-                    } else if (turn == 'white'){
-                        turn='black';
-                        tilexy=cells[i].id.split('cell',2)[1].split('-',2);
-                        boardLayout[parseInt(tilexy[0])][parseInt(tilexy[1])]=1;
-                        updateBoard();
-                        console.log(cells[i].id.split('cell',2)[1].split('-',2));
+                if (!cells[i].querySelector(".tile validMove")) {
+                    tilexy=cells[i].id.split('cell',2)[1].split('-',2);
+                    row = parseInt(tilexy[0]);
+                    column = parseInt(tilexy[1]);
+                    if (boardLayout[row][column] == 3) {
+                        if (turn == 'black') {
+                            boardLayout[row][column]=1;
+                            move=[row,column];
+                            captureTiles(move);
+                            turn='white';
+                            updateBoard();
+                            // canmove();
+                            // console.log(cells[i].id.split('cell',2)[1].split('-',2));
+                        } else if (turn == 'white'){
+                            boardLayout[row][column]=2;
+                            move=[row,column];
+                            captureTiles(move);
+                            turn='black';
+                            updateBoard();
+                            // canmove();
+                            // console.log(cells[i].id.split('cell',2)[1].split('-',2));
+                        }
                     }
                 }
                 //     tile=cells[i].querySelector('.tile');
@@ -120,26 +143,272 @@ window.addEventListener('load', () => {
             tile.classList.add('white');
         } else if (color == 'black'){
             tile.classList.add('black');
+        } else if (color == 'validMove'){
+            tile.classList.add('validMove');
         }
         return tile;
     }
 
+    function updateScore() {
+        let playerScoreUpdate = 0;
+        let AIScoreUpdate = 0;
+
+        for (let row = 0; row < 8; row++) {
+            for (let column = 0; column < 8; column++) {
+                let value = boardLayout[row][column];
+                if (value == 1) {
+                    playerScoreUpdate++;
+                } else if (value == 2) {
+                    AIScoreUpdate++;
+                }
+            }
+        }
+
+        playerScore.innerHTML = "You: " + playerScoreUpdate;
+        AIScore.innerHTML = "AI: " + AIScoreUpdate;
+
+    }
+
     function updateBoard() {
         board.innerHTML="";
+        resetValidMoves();
         createBoard();
+        checkValidMoves();
+        // console.log(boardLayout);
         for (let row = 0; row < 8; row++) {
             for (let column = 0; column < 8; column++) {
                 const currentTile = boardLayout[row][column];
-                if (currentTile == 0) {
+                if (currentTile == 3) {
+                    document.getElementById('cell' + row + '-' + column).appendChild(createTile('validMove'));
+                }else if (currentTile == 1){
+                    document.getElementById('cell' + row + '-' + column).appendChild(createTile('black'));
                 }
-                else {
-                    currentTile == 1 ? document.getElementById('cell' + row + '-' + column).appendChild(createTile('black')) : document.getElementById('cell' + row + '-' + column).appendChild(createTile('white'));
+                else if (currentTile == 2){
+                    document.getElementById('cell' + row + '-' + column).appendChild(createTile('white'));
+                }
+            }
+        }
+        
+        updateScore();
+
+    }
+
+    function resetValidMoves() {
+        for (let row = 0; row < 8; row++) {
+            for (let column = 0; column < 8; column++) {
+                if (boardLayout[row][column] == 3) {
+                    boardLayout[row][column] = 0;
                 }
             }
         }
     }
 
+    function canmove() {
+        move=false;
+        for (let row = 0; row < 8; row++) {
+            for (let column = 0; column < 8; column++) {
+                if (boardLayout[row][column] == 3) {
+                    move=true;
+                }
+            }
+        }
+        if (move==false) {
+            if (turn=='black') {
+                turn='white';
+                move=false;
+                for (let row = 0; row < 8; row++) {
+                    for (let column = 0; column < 8; column++) {
+                        if (boardLayout[row][column] == 3) {
+                            move=true;
+                        }
+                    }
+                }
+                if (move==false) {
+                    gameFinish();
+                }
+            }
+            if (turn=='white') {
+                turn='black';
+                move=false;
+                for (let row = 0; row < 8; row++) {
+                    for (let column = 0; column < 8; column++) {
+                        if (boardLayout[row][column] == 3) {
+                            move=true;
+                        }
+                    }
+                }
+                if (move==false) {
+                    gameFinish();
+                }
+            }
+        }
+    }
+
+    function checkValidMoves() {
+        for (let row = 0; row < 8; row++) {
+            for (let column = 0; column < 8; column++) {
+                cell=[row,column];
+                if (boardLayout[cell[0]][cell[1]] != 2 && boardLayout[cell[0]][cell[1]] != 1) {
+                    if (checkMoves(cell)) {
+                        boardLayout[cell[0]][cell[1]]=3;
+                    };
+                }
+            }
+        }
+    }
+
+    function checkMoves(move) {
+        for (let i = 0; i < 8; i++) {
+            result=false;
+            if (turn=='black') {
+                if ( !(move[0]+directions[i][0] < 0) && !(move[0]+directions[i][0] > 7) && !(move[1]+directions[i][1] < 0) && !(move[0]+directions[i][0] > 7)) {
+                    cellcheck=[move[0]+directions[i][0],move[1]+directions[i][1]];
+                    if (boardLayout[cellcheck[0]][cellcheck[1]] == 2) {
+                        if (checkDirections(directions[i],cellcheck)) {
+                            result=true;
+                            return result;
+                        }
+                    }
+                }
+            }else if (turn=='white') {
+                if ( !(move[0]+directions[i][0] < 0) && !(move[0]+directions[i][0] > 7) && !(move[1]+directions[i][1] < 0) && !(move[0]+directions[i][0] > 7)) {
+                    cellcheck=[move[0]+directions[i][0],move[1]+directions[i][1]];
+                    if (boardLayout[cellcheck[0]][cellcheck[1]] == 1) {
+                        if (checkDirections(directions[i],cellcheck)) {
+                            result=true;
+                            return result;
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+    function checkDirections(direction,move) {
+        check=false;
+        found=false;
+        rep=1;
+        while (check==false) {
+            if ( !(move[0]+(direction[0]*rep) < 0) && !(move[0]+(direction[0]*rep) > 7) && !(move[1]+(direction[1]*rep) < 0) && !(move[0]+(direction[1]*rep) > 7)) {
+                if (turn=='black') {
+                    cellcheck=[move[0]+(direction[0]*rep),move[1]+(direction[1]*rep)];
+                    if (boardLayout[cellcheck[0]][cellcheck[1]] == 1) {
+                        found=true;
+                        check=true;
+                    }else if ((boardLayout[cellcheck[0]][cellcheck[1]] == 0)){
+                        found=false;
+                        check=true;
+                    }else if ((boardLayout[cellcheck[0]][cellcheck[1]] == 3)){
+                        found=false;
+                        check=true;
+                    }
+                }else if (turn=='white') {
+                    cellcheck=[move[0]+(direction[0]*rep),move[1]+(direction[1]*rep)];
+                    if (boardLayout[cellcheck[0]][cellcheck[1]] == 2) {
+                        found=true;
+                        check=true;
+                    }else if ((boardLayout[cellcheck[0]][cellcheck[1]] == 0)){
+                        found=false;
+                        check=true;
+                    }else if ((boardLayout[cellcheck[0]][cellcheck[1]] == 3)){
+                        found=false;
+                        check=true;
+                    }
+                }
+            }else {
+                found=false;
+                check=true
+            }
+            rep++
+            if (rep==8) {
+                found=false;
+                check=true;
+            }
+        }
+        return found;
+    }
+
+    function checkMove(move) {
+        // console.log(move);
+        for (let i = 0; i < 8; i++) {
+            if (turn=='black') {
+                if ( !(move[0]+directions[i][0] < 0) && !(move[0]+directions[i][0] > 7) && !(move[1]+directions[i][1] < 0) && !(move[0]+directions[i][0] > 7)) {
+                    cellcheck=[move[0]+directions[i][0],move[1]+directions[i][1]];
+                    if (boardLayout[cellcheck[0]][cellcheck[1]] == 2) {
+                        // console.log(move);
+                        checkDirection(directions[i],move);
+                    }
+                }
+            }else if (turn=='white') {
+                if ( !(move[0]+directions[i][0] < 0) && !(move[0]+directions[i][0] > 7) && !(move[1]+directions[i][1] < 0) && !(move[0]+directions[i][0] > 7)) {
+                    cellcheck=[move[0]+directions[i][0],move[1]+directions[i][1]];
+                    if (boardLayout[cellcheck[0]][cellcheck[1]] == 1) {
+                        checkDirection(directions[i],move);
+                    }
+                }
+            }
+        }
+    }
+
+    function checkDirection(direction,move) {
+        check=false;
+        found=false;
+        rep=1;
+        while (check==false) {
+            if ( !(move[0]+(direction[0]*rep) < 0) && !(move[0]+(direction[0]*rep) > 7) && !(move[1]+(direction[1]*rep) < 0) && !(move[0]+(direction[1]*rep) > 7)) {
+                // console.log(move);
+                if (turn=='black') {
+                    cellcheck=[move[0]+(direction[0]*rep),move[1]+(direction[1]*rep)];
+                    if (boardLayout[cellcheck[0]][cellcheck[1]] == 1) {
+                        found=true;
+                        boardLayout[move[0]][move[1]]=1;
+                        for (let i = 0; i < rep; i++) {
+                            cellcapture=[move[0]+(direction[0]*i),move[1]+(direction[1]*i)];
+                            console.log(cellcapture);
+                            boardLayout[cellcapture[0]][cellcapture[1]]=1;
+                            console.log(boardLayout);
+                        }
+                        check=true;
+                    }else if ((boardLayout[cellcheck[0]][cellcheck[1]] == 0)){
+                        found=false;
+                        check=true;
+                    }else if ((boardLayout[cellcheck[0]][cellcheck[1]] == 3)){
+                        found=false;
+                        check=true;
+                    }
+                }else if (turn=='white') {
+                    cellcheck=[move[0]+(direction[0]*rep),move[1]+(direction[1]*rep)];
+                    if (boardLayout[cellcheck[0]][cellcheck[1]] == 2) {
+                        found=true;
+                        boardLayout[move[0]][move[1]]=2;
+                        for (let i = 0; i < rep; i++) {
+                            cellcapture=[move[0]+(direction[0]*i),move[1]+(direction[1]*i)];
+                            console.log(cellcapture);
+                            boardLayout[cellcapture[0]][cellcapture[1]]=2;
+                            console.log(boardLayout);
+                        }
+                        check=true;
+                    }else if ((boardLayout[cellcheck[0]][cellcheck[1]] == 0)){
+                        found=false;
+                        check=true;
+                    }else if ((boardLayout[cellcheck[0]][cellcheck[1]] == 3)){
+                        found=false;
+                        check=true;
+                    }
+                }
+            }else {
+                found=false;
+                check=true
+            }
+            rep++
+        }
+    }
+
+    function captureTiles(move) {
+        checkMove(move);
+    }
+
     setInterval(createLine, 750);
     updateBoard();
-
-})
+});
